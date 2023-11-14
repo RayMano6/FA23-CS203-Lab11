eb0bcd79af066a006d69ee9c76e520b83d74d795
RayMano6 <150363893+RayMano6@users.noreply.github.com>
2023-11-14 16:07:22 -0600

    Here are my final Changes!

--- src/Lab12/Encrypter.java
+++ src/Lab12/Encrypter.java
@@ -44,16 +44,45 @@ public class Encrypter {
 		try(Scanner fileScanner = new Scanner(Paths.get(inputFilePath))){
     		while(fileScanner.hasNextLine()) {
     			
-    			String line = fileScanner.nextLine().toLowerCase();
+    			String line = fileScanner.nextLine();
     			char[] arr = line.toCharArray();
     			for(int i=0; i<arr.length;i++) {
-    				if (arr[i]>='a' + 10 || arr[i]<='z' +  10) {
-//    					arr[i]+=32;
-    					arr[i]+=shift;
+    				System.out.println(arr[i]);
+    				if ((arr[i]<97+shift && arr[i]>97)|| arr[i]>122) {
+    					int newarraydecimalval = 122 - ((shift-(arr[i]-97))-1);
+    					int newshift1=newarraydecimalval-arr[i];
+    					arr[i]+= newshift1;
+//    					int Shiftremaining = arr[i]-'a';
+//    					int z =('z'-shift - Shiftremaining -1);
+////    					arr[i]+=32;
+//    					shift = z-arr[i];
+//    					arr[i]+=shift; 
+    					
+//    					System.out.println(arr[2]-1);
+//    				
+    					
+//    					arr[i]+=z;
     				}
-    				else if (arr[i]>='A' + 10 || arr[i]<='Z' + 10) {
-    					arr[i]+=shift;
+    				else if (arr[i]=='a' || arr[i]=='A') {
+    					arr[i]+=22;
     				}
+    				else if (arr[i]>=97+shift && arr[i]<=122) {
+    					arr[i]-= shift;
+    				}
+    				else if ((arr[i]<65+shift && arr[i]>65) || (arr[i]>90 && arr[i]<97)) {
+    					int V = 90 - (shift-(arr[i]-65)-1);
+    					int newshift2 = V-arr[i];
+    					arr[i]+=newshift2;
+    				
+    					
+    				}
+    				else if (arr[i]>=65+shift && arr[i]<=90) {
+    					arr[i]-= shift;
+    				}
+    				
+//    				System.out.println(arr[i]-=arr[i]);
+//    				System.out.println(shift);
+    				
     				
     				
 //    			    char a = inputFilePath.charAt(i);
@@ -62,7 +91,7 @@ public class Encrypter {
     			String str = new String(arr);
 //    			System.out.println(str);
     			output.println(str);
-    			System.out.println(line);
+//    			System.out.println(line);
     		}
     	}
     	} catch (FileNotFoundException e) {
@@ -83,7 +112,7 @@ public class Encrypter {
      * @throws Exception if an error occurs while reading or writing the files
      */
     public void decrypt(String messageFilePath, String decryptedFilePath) throws Exception {
-    		shift = - shift;
+    		shift = + shift;
     		encrypt( messageFilePath,  decryptedFilePath);
         //TODO: Call the read method, decrypt the file contents, and then write to new file
     }
